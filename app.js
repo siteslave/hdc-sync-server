e3e76a0317423b707c5bc0e8959f3edfcb46a852
Satit Rianpit <rianpit@gmail.com>
2015-07-20 15:09:08 +0700

    change default uploaded folder

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(multer({
-  dest: './uploaded',
+  dest: '../uploaded',
   rename: function (fieldname, filename) {
     return filename.replace(/\W+/g, '-').toLowerCase() + '-' + Date.now()
   }
